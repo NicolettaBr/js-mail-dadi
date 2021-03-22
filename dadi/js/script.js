69f292e837b759b30032dd568fa96268b194ae36
Nicoletta Berrettoni <nico.berrettoni@gmail.com>
2021-03-22 19:55:48 +0100

    inizio secondo esercizio

---
+++ dadi/js/script.js
@@ -0,0 +1,11 @@
+//Generare un numero random da 1 a 6, sia per il giocatore sia per il computer
+//Stabilire il vincitore in base a chi fa il punteggio più alto
+
+//crea array di numeri da 1 a 6
+
+var arrayNumeri = [1, 2, 3, 4, 5, 6];
+console.log(arrayNumeri);
+
+//genera 2 numeri random,  uno per il giocatore uno per il pc
+
+//numero più alto vince
